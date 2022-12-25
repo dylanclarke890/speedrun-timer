8690f2598044141a5379358610d0143e532c01f8
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-25 03:00:17 +0000

    started work on UI

--- assets/js/timeit.js
+++ assets/js/timeit.js
@@ -3,7 +3,7 @@ UI.onPageReady(() => {
 
   const testGetBtn = document.getElementById("testGet");
   UI.addEvent(testGetBtn, "click", async () => {
-    const runData = await client.run.get("9okq");
+    const runData = await client.run.get("6wb4");
     const fetchedRun = SplitsIORun.from(runData.run);
     window.run = TimeItSpeedRun.from(fetchedRun);
   });
